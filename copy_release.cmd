5547c2422eb0145676b03aac6aa35f3de5f52fb8
Tim Graupmann <tgraupmann@gmail.com>
2017-08-28 20:23:55 -0700

    Added script to copy release

---
+++ copy_release.cmd
@@ -0,0 +1,4 @@
+COPY /Y "Release\CChromaRuntime.dll" "C:\Public\UnityNativeChromaSDK\Assets\Plugins\UnityNativeChromaSDK3.dll"
+COPY /Y "Release\CChromaRuntime.dll" "C:\Public\UnityNativeChromaSDK\Assets\Plugins\x86\UnityNativeChromaSDK.dll"
+COPY /Y "x64\Release\CChromaRuntime.dll" "C:\Public\UnityNativeChromaSDK\Assets\Plugins\x64\UnityNativeChromaSDK.dll"
+PAUSE
